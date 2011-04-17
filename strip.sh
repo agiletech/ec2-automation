8a85014002786672488cd61dc59ab5e134ac7762
Romans Malinovskis <r@agiletech.ie>
2011-04-17 19:31:08 +0100

    strip more

--- strip.sh
+++ strip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -vE '^/var/tmp|^/usr/share|^/usr/lib|^/usr/include|^/usr/bin|^/var/db/pkg|^/etc/ld.so.cache|^/etc/csh.env|^/etc/profile.env|^/var/cache/edb'
+grep -vE '^/var/tmp|^/usr/share|^/usr/lib|^/usr/include|^/usr/bin|^/var/db/pkg|^/etc/ld.so.cache|^/etc/csh.env|^/etc/profile.env|^/var/cache/edb|^/usr/sbin|^/lib/|^/var/cache/ldconfig'
